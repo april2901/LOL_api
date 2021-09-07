634fc75b919fb2328927e222bded7698d8a5184a
april2901 <april2901@naver.com>
2021-09-08 03:20:51 +0900

    ingame or not, gamemode

--- LOL.py
+++ LOL.py
@@ -1,16 +1,34 @@
 import requests
 import time
 
-api_key='RGAPI-7dbe35da-b14f-415b-8d05-9c6867ac5cf3'
+api_key='RGAPI-7d0b0900-f58c-4de8-8e95-4d6eb2fc959a'
 
 
-name="april2901"
-a="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+'?api_key='+api_key
-r=requests.get(a)
-id=r.json()['id']
+namelist=["로지택키보드","샘호네모동그라미"]
+
 while(1):
-    b="https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/"+id+"?api_key="+api_key
-    r=requests.get(b)
-    sta=r.json()['status']
-    print(sta)
-    time.sleep(3)
+    for name in namelist:
+        a="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+'?api_key='+api_key
+        r=requests.get(a)
+        id=r.json()['id']
+        b="https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/"+id+"?api_key="+api_key
+        r=requests.get(b)
+        try:
+            r.json()['gameId']
+        except KeyError:
+            print(name+': X    ', end='')
+            time.sleep(3)
+            continue
+    
+        gamemode=r.json()['gameMode']
+        if(gamemode=='ARAM'):
+            gm='칼바람'
+        elif(gamemode=="CLASSIC"):
+            gm='협곡'
+        elif(gamemode=="URF"):
+            gm='우르프'
+        else:
+            gm='??'
+        print(name+': '+gm+'     ', end='')
+        time.sleep(3)
+        print()
